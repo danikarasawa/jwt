ff9c4523db89cdd2955056ab89da8bd77539229a
danikarasawa <danikarasawa@gmail.com>
2019-11-28 22:15:21 -0300

    mongodb

---
+++ mongodb.js
@@ -0,0 +1,20 @@
+// exports.get = (req, res) => {
+//     const filter = req.query
+//     ClientRectList.find(filter)
+//     .then((result)) => {
+//         res.status(200).json(result);
+//     }
+//     .catch(err => res.status(500).send(err));
+// }
+
+
+// cliente.save()
+// .then(() => {
+//     res.status(201).send({
+//         status: true,
+//         message: "Cliente incluído"
+//     });
+// })
+// .catch(err => res.status(500).send(err));
+
+//THEN + CATCH OU PROMISES
